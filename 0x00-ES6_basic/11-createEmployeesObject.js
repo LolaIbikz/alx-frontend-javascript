81466cf6453c0a75a0f05a771356bc20db7b1e43
lolaIbikz <ibikunle.oluwafunmilola@gmail.com>
2023-06-29 21:35:41 +0000

    task basic commit

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,3 @@
+Latest commitexport default function createEmployeesObject(departmentName, employees) {
+  return { [departmentName]: [...employees] };
+}
---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+Latest commitexport default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+  };
+}
